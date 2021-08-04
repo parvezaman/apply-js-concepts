1975f35600fb225ff91f74c270bf8da057618801
parvezaman <emailtoamanullah@gmail.com>
2021-08-04 06:48:51 +0600

    let and const

---
+++ recap.js
@@ -0,0 +1,16 @@
+// let is changable
+let price1 = 23;
+price1 = 25;
+price1 = 56;
+
+console.log(price1);
+
+
+
+// const is non changable
+const price2 = 56;
+price2 = 53;
+
+console.log(price2);
+
+// TypeError: Assignment to constant variable.
